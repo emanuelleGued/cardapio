c4caa1f3233fd24897fed666a818608b78f22035
Emanuelle Garcia Guedes <garcia.emanuelle@academico.ifpb.edu.br>
2023-12-28 13:52:32 -0300

    atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,32 @@ import { useState } from 'react'
 import './App.css'
 import { Card } from './components/card/card';
 import { FoodData } from './interface/FoodData';
+import { useFoodData } from './components/hooks/useFoodsData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
-  const data: FoodData[] =  []; 
+  const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
-
-    <div className='container'>
-      <h1>Cardapio</h1>
+    <div className="container">
+      <h1>Cardápio</h1>
       <div className="card-grid">
-        {data.map(foodData => 
-          <Card 
+        {data?.map(foodData => 
+          <Card
             price={foodData.price} 
             title={foodData.title} 
             image={foodData.image}
-          /> 
+          />
         )}
       </div>
-       
+      {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
-      
-  
   )
 }
 
